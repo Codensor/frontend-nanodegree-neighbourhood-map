40c740b8911cffb025f7c963757b59f123c9e586
Abhilash Deshpande <abhilashadd@gmail.com>
2017-12-02 20:55:18 +0530

    Improved code structure

--- js/app.js
+++ js/app.js
@@ -1,34 +1,35 @@
-let map;	// holds google map
-let singapore = {	// holds lat lng of the region of interest
+let map; // holds google map
+let singapore = { // holds lat lng of the region of interest
 	lat: 1.3521,
 	lng: 103.8198
 };
-let coordinates = [];	// used to hold coordinates of places retrieved from local storage
+let coordinates = []; // used to hold coordinates of places retrieved from local storage
+let loaderStatus = ko.observable(false);
 //	checking if local storage has coordinates of places
 if (localStorage.coordinates) {
 	coordinates = JSON.parse(localStorage.getItem("coordinates"));
-} else {	// loading the loadinng screen since no local strorage
-	$("#loading").toggleClass("hide");
-	$(".info-close-btn").toggleClass("hide");
-	$("#information").toggleClass("hide");
-	$("#infodesk").toggleClass("hide-infodesk");
+} else { // loading the loadinng screen since no local strorage
+	loaderStatus(true);
 }
 let hangouts = ["Universal Studios Singapore", "Sentosa Island", "Singapore Zoo", "Night Safari", "Jurong Bird Park",
 	"Burmese Buddhist Temple", "Gardens By The Bay", "Orchard Road", "Singapore Botanic Gardens", "Chinatown",
 	"Singapore Flyer", "Sands Skypark", "Clarke Quay", "Helix Bridge", "Fountain of Wealth",
 	"Sri Mariamman Temple", "Raffles Hotel", "ArtScience Museum", "Buddha Tooth Relic Temple and Museum"
-];		// holds the names of the places to load
-let addressPos;	// used to hold name of the place while geocoding using google
-let marker;	// used to intialize each google map marker
-let markers = [];	// used to hold all the google map markers
-let currentMarker = null;	// used to hold the current active google map marker
-let currentPointer = 0;	// used to diffrentiate between active and newly activated google map markers
-let infoWindow;	// used to initialize google maps marker info windows
-let placeName = ko.observable();	// used to hold currently activated google map maker's venue name
-let imgurl = ko.observable();	// used to hold currently activated google map maker's venue image url
-let desc = ko.observable();	// used to hold currently activated google map maker's venue description
-let placeAddress = ko.observable();	// used to hold currently activated google map maker's venue address
-let placeContact = ko.observable();	// used to hold currently activated google map maker's venue contact number
+]; // holds the names of the places to load
+let addressPos; // used to hold name of the place while geocoding using google
+let marker; // used to intialize each google map marker
+let markers = []; // used to hold all the google map markers
+let markersCopy = [];	// // used to hold a copy of all the google map markers
+let currentMarker = null; // used to hold the current active google map marker
+let currentPointer = 0; // used to diffrentiate between active and newly activated google map markers
+let infoWindow; // used to initialize google maps marker info windows
+let infoHolder = `<a id="infoWindow" href="#" onclick="loadMoreInfo()">More Info Here</a>`; // holds google maps info window markup
+let infoStatus = ko.observable(false); // used to store more infowindow state i.e oprn or close
+let placeName = ko.observable(); // used to hold currently activated google map maker's venue name
+let imgurl = ko.observable(); // used to hold currently activated google map maker's venue image url
+let desc = ko.observable(); // used to hold currently activated google map maker's venue description
+let placeAddress = ko.observable(); // used to hold currently activated google map maker's venue address
+let placeContact = ko.observable(); // used to hold currently activated google map maker's venue contact number
 
 // function to handle google maps api load error
 function googleMapLoadFail(status) {
@@ -73,11 +74,7 @@ function geocodeAddress(geocoder, resultsMap, addressPos) {
 					localStorage.setItem("coordinates", JSON.stringify(coordinates));
 				}, 5000);
 				placeMarkers();
-				// removing the loading screen
-				$("#infodesk").toggleClass("hide-infodesk");
-				$("#loading").toggleClass("hide");
-				$(".info-close-btn").toggleClass("hide");
-				$("#information").toggleClass("hide");
+				loaderStatus(false);
 			}
 		} else if (status === google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
 			setTimeout(function() {
@@ -137,7 +134,7 @@ function markerEvents() {
 function makeInfoWindow(mark) {
 	let origin = 2;
 	infoWindow = new google.maps.InfoWindow({
-		content: `<div><h3>${mark.getTitle()}</h3><a id="infoWindow" href="#">More Info Here</a></div>`
+		content: `<div><h3>${mark.getTitle()}</h3>${infoHolder}</div>`
 	});
 	infoWindow.open(map, mark)
 	infoWindow.addListener('closeclick', function() {
@@ -154,11 +151,6 @@ function closeInfoWindow(mark, source) {
 		currentPointer = 0;
 	}
 }
-
-// event listener to load information about a place
-$("#map").on("click", "a#infoWindow", function() {
-	loadMoreInfo();
-});
 
 // function to retrieve venue details from foursquare
 function loadMoreInfo() {
@@ -204,7 +196,7 @@ function populateInfo(result) {
 		placeContact("Contact Not Found");
 	}
 	setTimeout(function() {
-		$("#infodesk").toggleClass("hide-infodesk");
+		infoStatus(true);
 	}, 200);
 };
 
@@ -223,22 +215,14 @@ function setFilterList(list) {
 	}
 }
 
-// function to open and close the sidebar
-function toggleSidebar() {
-	$(".hamburger").toggleClass("hide");
-	$(".close-btn").toggleClass("hide");
-	$("#sidebar").toggleClass("hide-sidebar");
-	$("#filterbox").val("Filter...");
-	$("#filterbox").focus(function() {
-		$(this).val("");
-	});
-}
-
 // function to perform filtering of the list of places in the sidebar
 function applyFilter(list) {
+	infoWindow.close();
+	currentMarker.setAnimation(null);
+	currentMarker = null;
+	currentPointer = 0;
 	let tempArray = [];
 	let str = $("#filterbox").val();
-	console.log(str);
 	for (let i = 0; i < list.filterList().length; i++) {
 		let state = list.filterList()[i].name.toLowerCase().includes(str);
 		if (state) {
@@ -261,8 +245,10 @@ function applyFilter(list) {
 }
 
 // knockout viewmodel
-const ViewModel = function() {
+let ViewModel = function() {
 	const self = this;
+	self.sidebarStatus = ko.observable(false); // used to hold sidebar state i.e open or close
+	self.filterText = ko.observable("Filter..."); // holds the default text for the filter inputbox
 	self.filterList = ko.observableArray([]); // used to store place names that are to be shown in the sidebar
 
 	// function to set the values of the filterList
@@ -274,7 +260,13 @@ const ViewModel = function() {
 
 	// function to interact with the sidebar
 	self.toggle = function() {
-		toggleSidebar();
+		if (self.sidebarStatus() === false) {
+			self.sidebarStatus(true);
+			self.filterText("Filter...");
+		} else {
+			self.sidebarStatus(false);
+			self.filterText("Filter...");
+		}
 	};
 
 	// function to load the filter functionality
@@ -290,11 +282,12 @@ const ViewModel = function() {
 
 	// function to close the place information window
 	self.closeMoreInfo = function() {
-		$("#infodesk").toggleClass("hide-infodesk");
+		infoStatus(false);
 	};
 
 	// function to reset the sidebar places list after performing a filter
 	self.reset = function() {
+		self.filterText("Filter...");
 		self.setFilter();
 	};
 }
